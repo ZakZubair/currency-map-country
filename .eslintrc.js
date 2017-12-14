4e9ace8215d72fd062c547237a06142c63c9f6b2
Zak Zubair <zakaaf@hotmail.co.uk>
2017-12-14 11:08:04 +0000

    Improved the eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "node":  true,
+        "jest": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [2, 4],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     verbose: true,
-    coveragePathIgnorePatterns: ["./lib/"]
+    coveragePathIgnorePatterns: ['./lib/'],
+    testPathIgnorePatterns: ['./lib/']
 };
